2732d87ad2198f7672a05ec07011b4d72d731025
Manny <casta.ma502@gmail.com>
2025-03-30 14:57:44 -0700

    Eslint is working properly.

---
+++ sam-fridge/eslint.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node, // This adds Node globals like process, __dirname, etc.
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      // Add any additional server-side rules as needed.
+    },
+  },
+]);
--- sam-fridge/src/handlers/get-by-id.mjs
+++ sam-fridge/src/handlers/get-by-id.mjs
@@ -14,18 +14,20 @@ const tableName = process.env.SAMPLE_TABLE;
  */
 export const getByIdHandler = async (event) => {
   if (event.httpMethod !== 'GET') {
-    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+    throw new Error(
+      `getMethod only accept GET method, you tried: ${event.httpMethod}`
+    );
   }
   // All log statements are written to CloudWatch
   console.info('received:', event);
- 
+
   // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
   const id = event.pathParameters.id;
- 
+
   // Get the item from the table
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
   var params = {
-    TableName : tableName,
+    TableName: tableName,
     Key: { id: id },
   };
 
@@ -33,15 +35,17 @@ export const getByIdHandler = async (event) => {
     const data = await ddbDocClient.send(new GetCommand(params));
     var item = data.Item;
   } catch (err) {
-    console.log("Error", err);
+    console.log('Error', err);
   }
- 
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify(item)
+    body: JSON.stringify(item),
   };
- 
+
   // All log statements are written to CloudWatch
-  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
   return response;
-}
+};
